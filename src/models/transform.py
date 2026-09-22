e6496b141e23b78fd87bdd479bb968d6c83c3097
Daniele <daniele.daccordo@studenti.unipd.it>
2026-09-22 19:56:45 +0200

    fix 2 added transform as parameter: transform as a function, no more as a class

--- src/models/transform.py
+++ src/models/transform.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-class DopplerTraceTransformation:
-    def __init__(self):
-        None
-
-    def __call__(self, window):
-        noise = np.random.randn(*window.shape) * 0.1
-        return np.clip(window + noise, 0, 1)
+def dopplerTraceTransformation(window):
+    noise = np.random.randn(*window.shape) * 0.1
+    return np.clip(window + noise, 0, 1)
