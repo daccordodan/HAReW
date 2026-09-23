37546735298aa52fcb664bcb03435c73a33426f8
Daniele <daniele.daccordo@studenti.unipd.it>
2026-09-23 19:22:41 +0200

    fixed transform

--- src/models/transform.py
+++ src/models/transform.py
@@ -2,43 +2,44 @@ import numpy as np
 from scipy.interpolate import interp1d
 
 def dopplerTraceTransformation(window):
-    p=0.7
-    augmented = doppler_shift(p, window, shift_bins=5)
-    augmented = time_stretch(p, augmented, scale_range=(0.85, 1.15))
-    augmented = gaussian_noise(p, augmented, noise_std=0.08)
-    augmented = temporal_mask(p, augmented, mask_width_range=(3, 10))
-    augmented = frequency_mask(p, augmented, mask_width_range=(2, 6))
+    p=0.6
+    augmented = doppler_shift(p, window, shift_bin=5)
+    augmented = time_stretch(p, augmented, scale_range=(0.9, 1.1))
+    augmented = gaussian_noise(p, augmented, noise_std=0.04)
+    augmented = temporal_mask(p, augmented, mask_width=(3, 10), n_masks=4)
+    augmented = frequency_mask(p, augmented, mask_width=(2, 5), n_masks=3)
     return augmented
 
 def doppler_shift(p, trace, shift_bin):
     if np.random.rand() > p:
         return trace
-    shift = np.random.randInt(-shift_bin,shift_bin+1)
-    shifted = np.roll(trace, shift, axis=0)
+    shift = np.random.randint(-shift_bin,shift_bin+1)
+    shifted = np.roll(trace, shift, axis=1)
 
     if shift > 0:
-        shifted[:shift, :]=0
+        shifted[:, :shift, :]=trace.min()
     elif shift < 0:
-        shifted[shift:, :]=0
+        shifted[:, shift:, :]=trace.min()
     return shifted
 
 def time_stretch(p, trace, scale_range):
     if np.random.rand() > p:
         return trace
     scale = np.random.uniform(scale_range[0], scale_range[1])
-    fb, ts = trace.shape
-
-    time_ax_old=np.arrange(ts)
+    ch, ts, fb = trace.shape
+    
+    time_ax_old=np.arange(ts)
     time_ax_new=np.linspace(0, ts-1, int(ts/scale))
-    stretched = np.zeros((fb,len(time_ax_new)))
-    for i in range(fb):
-        stretched[i]=interp1d(time_ax_old, trace[i,:], kind="linear", bounds_error=False, fill_value=0)(time_ax_new)
+    stretched = np.zeros((ch, len(time_ax_new), fb))
+    for c in range(ch):
+        for i in range(fb):
+            stretched[c,:,i]=interp1d(time_ax_old, trace[c,:,i], kind="linear", bounds_error=False, fill_value=0)(time_ax_new)
 
     if stretched.shape[1] < ts:
-        pad_width = ((0, 0), (0, ts - stretched.shape[1]))
-        stretched = np.pad(stretched, pad_width)
+        pad_width = ((0, 0), (0, ts - stretched.shape[1]), (0, 0))
+        stretched = np.pad(stretched, pad_width, "constant", constant_values=stretched.min())
     else:
-        stretched = stretched[:, :ts]
+        stretched = stretched[:, :ts, :]
     return stretched
 
 def gaussian_noise(p, trace, noise_std=0.05):
@@ -56,9 +57,9 @@ def temporal_mask(p, trace, mask_width, n_masks):
     ts=trace.shape[1]
     n_masks=np.random.randint(1, n_masks)
     for _ in range(n_masks):
-        mask_width=np.random.randint(n_masks[0], n_masks[1])
-        start_time=np.random.randint(0, max(1, ts-mask_width))
-        masked[:, start_time:start_time+mask_width]=0
+        width=np.random.randint(mask_width[0], mask_width[1])
+        start_time=np.random.randint(0, max(1, ts-width))
+        masked[:, start_time:start_time+width, :]=trace.min()
     return masked
 
 def frequency_mask(p, trace, mask_width, n_masks):
@@ -66,10 +67,10 @@ def frequency_mask(p, trace, mask_width, n_masks):
         return trace
 
     masked=trace.copy()
-    ts=trace.shape[0]
+    fb=trace.shape[2]
     n_masks=np.random.randint(1, n_masks)
     for _ in range(n_masks):
-        mask_width=np.random.randint(n_masks[0], n_masks[1])
-        start_time=np.random.randint(0, max(1, ts-mask_width))
-        masked[start_time:start_time+mask_width, :]=0
+        width=np.random.randint(mask_width[0], mask_width[1])
+        start_freq=np.random.randint(0, max(1, fb-width))
+        masked[:, :, start_freq:start_freq+width]=trace.min()
     return masked
