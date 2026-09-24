f5ae7e4a4f9730f6fe7076851a876a0a02f6e88d
Daniele <daniele.daccordo@studenti.unipd.it>
2026-09-24 20:26:29 +0200

    Evaluation changes

--- src/evaluation/evaluation_utils.py
+++ src/evaluation/evaluation_utils.py
@@ -15,12 +15,7 @@ from src.data.label_mapping import TARGET_CLASSES
 from src.models.sharp_classifier import SHARPClassifier
 from huggingface_hub import hf_hub_download
 
-def load_checkpoint_to_model(config, checkpoint_name, device, logger, local=False):
-    model=SHARPClassifier(
-        n_classes=len(TARGET_CLASSES),
-        nw=config["doppler"]["stacked_vectors_nw"],
-        nd=config["doppler"]["velocity_bins_nd"],
-    ).to(device)
+def load_checkpoint_to_model(config, model, checkpoint_name, device, logger, local=False):
     if local:
         checkpoint_path = Path(checkpoint_name)
         if not checkpoint_path.is_absolute() and not checkpoint_path.exists():
